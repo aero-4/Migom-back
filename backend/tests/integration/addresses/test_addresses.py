bd196c0d4ab2bea862f26ef86923776f636d571c
zangetsu <dimongames6@gmail.com>
2025-11-21 00:42:52 +0300

    integration test: not auth - add address

--- backend/tests/integration/addresses/test_addresses.py
+++ backend/tests/integration/addresses/test_addresses.py
@@ -16,3 +16,13 @@ async def test_success_add_address(clear_db, address_factory, user_factory):
 
         address_data = AddressCreateDTO(city="Москва", street="Колотушкина", house_number=123)
         await address_factory(client, address_data)
+
+
+@pytest.mark.asyncio
+async def test_add_address_not_authenticated(clear_db, address_factory):
+    async with httpx.AsyncClient(base_url="http://localhost:8000") as client:
+        address_data = AddressCreateDTO(city="Москва", street="Колотушкина", house_number=123)
+        response = await client.post("/api/addresses/", json=address_data.model_dump(mode="json"))
+
+        assert response.status_code == 401
+        assert response.json() == {"detail": "User not authenticated"}
