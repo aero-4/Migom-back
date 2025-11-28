b9fd2ec6106dc53eb6e18023ea6dff01e540deee
zangetsu <dimongames6@gmail.com>
2025-11-28 21:24:40 +0300

    payments

--- backend/src/payments/presentation/api.py
+++ backend/src/payments/presentation/api.py
@@ -5,6 +5,7 @@ from src.auth.presentation.dependencies import TokenAuthDep
 from src.orders.domain.entities import Order
 from src.orders.presentation.dtos import OrderCreateDTO
 from src.payments.application.use_cases.add_payment import add_payment
+from src.payments.application.use_cases.get_callback import get_payment_callback
 
 payments_api_router = APIRouter()
 
@@ -14,21 +15,21 @@ async def add(method: str, order: Order, auth: TokenAuthDep):
     return await add_payment(order, auth, method)
 
 
-# @payments_api_router.callbacks("/callback")
-# async def callback(request: Request, auth: TokenAuthDep):
-#     return await get_callback_payment(request)
+@payments_api_router.callbacks("/callback")
+async def callback(request: Request, auth: TokenAuthDep):
+    return await get_payment_callback(request)
 
-#
-# @payments_api_router.get("/{order_id}")
-# async def get_all(order_id: int):
-#     return {"msg": "Payment get all"}
-#
-#
-# @payments_api_router.patch("/{order_id}")
-# async def update(order_id: int):
-#     return {"msg": "Payment updated"}
-#
-#
-# @payments_api_router.delete("/{order_id}")
-# async def delete(order_id: int):
-#     return {"msg": "Payment deleted"}
+
+@payments_api_router.get("/{order_id}")
+async def get_all(order_id: int):
+    return {"msg": "Payment get all"}
+
+
+@payments_api_router.patch("/{order_id}")
+async def update(order_id: int):
+    return {"msg": "Payment updated"}
+
+
+@payments_api_router.delete("/{order_id}")
+async def delete(order_id: int):
+    return {"msg": "Payment deleted"}
